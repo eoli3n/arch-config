ba44c9eb906afaad0f8c5c1b9c92bc7bad5b447d
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-04-21 16:30:53 +0200

    fix tipo

--- scripts/install/01-configure.sh
+++ scripts/install/01-configure.sh
@@ -72,7 +72,7 @@ mkfs.btrfs -L "Sun" $BTRFS
 
 # Create BTRFS subvolumes
 print "Create subvolumes"
-mount -t btrfs $BTFRS /mnt
+mount -t btrfs $BTRFS /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@snapshots
