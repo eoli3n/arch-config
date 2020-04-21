267e06c647c6df2db38bc9ef97d0826720021484
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-04-21 18:00:04 +0200

    fix grub install

--- scripts/install/02-install.sh
+++ scripts/install/02-install.sh
@@ -46,7 +46,7 @@ hostname=$hostname arch-chroot /mnt /bin/bash -e <<"EOF"
   # Install grub2
   pacman -S grub efibootmgr grub-btrfs --noconfirm
   grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-  mkdir -p $boot/EFI/boot
+  mkdir -p /boot/EFI/boot
   cp /boot/EFI/grub_uefi/grubx64.efi /boot/EFI/boot/bootx64.efi
   grub-mkconfig -o /boot/grub/grub.cfg
 EOF
