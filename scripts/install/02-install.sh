483bda86db2a5893607c9188f50cd78094e385ab
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-04-21 17:14:55 +0200

    upgarde install

--- scripts/install/02-install.sh
+++ scripts/install/02-install.sh
@@ -11,15 +11,19 @@ print "Sort mirrors"
 pacman -Sy pacman-contrib --noconfirm
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+print "Sorting..."
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 # Install
+print "Install Archlinux"
 pacstrap /mnt base linux linux-firmware btrfs-progs ansible git
 
 # Generate Fstab
+print "Generate fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot
+print "Chroot and configure system"
 arch-chroot /mnt /bin/bash -e <<EOF
   
   # Set hostname
