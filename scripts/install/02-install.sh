f57f0d52eadca6894ba3ab9aa79526186bfd7586
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-04-21 17:09:39 +0200

    install pacman contrib package

--- scripts/install/02-install.sh
+++ scripts/install/02-install.sh
@@ -8,6 +8,7 @@ print () {
 
 # Sort mirrors
 print "Sort mirrors"
+pacman -Sy pacman-contrib --noconfirm
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
