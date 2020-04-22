25567dabab03e882776b04502e83ec0810b4a4c2
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-04-22 11:32:18 +0200

    forcing root kernel parameter

--- scripts/install/02-install.sh
+++ scripts/install/02-install.sh
@@ -78,7 +78,7 @@ arch-chroot /mnt /bin/bash -xe <<"EOF"
 
   # Prepare grub2
   #sed -i 's/#\(GRUB_ENABLE_CRYPTODISK=y\)/\1/' /etc/default/grub
-  sed -i 's-\(GRUB_CMDLINE_LINUX=\).*-\1"resume=/dev/mapper/swap"-' /etc/default/grub
+  sed -i 's-\(GRUB_CMDLINE_LINUX=\).*-\1"root=/dev/mapper/universe resume=/dev/mapper/swap"-' /etc/default/grub
 
   # Install grub2
   grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
