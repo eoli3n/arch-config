f1a97595cf506c76a3766e269fc646d41548eaed
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-05-03 01:47:47 +0200

    fixed recover scripts

--- scripts/recover/01-mount.sh
+++ scripts/recover/01-mount.sh
@@ -6,20 +6,16 @@ print () {
     echo -e "\n\033[1m> $1\033[0m\n"
 }
 
-# Import zpool
 print "Import zpool"
 zpool import -d /dev/disk/by-id -R /mnt -l zroot
 
-# Enable SWAP
 print "Enable SWAP"
 swapon /dev/zvol/zroot/swap
 
-# Mount EFI part
 print "Mount EFI part"
 mount $EFI /mnt/boot
 
-# Mount home part
-print "Mount EFI part"
+print "Mount home part"
 mount -t zfs zroot/data/home /mnt/home
 
 # Finish
