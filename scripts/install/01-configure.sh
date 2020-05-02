8d9023cb8775d9678ba676e74ee571675b478b9b
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-05-02 23:54:20 +0200

    fix home mount

--- scripts/install/01-configure.sh
+++ scripts/install/01-configure.sh
@@ -128,7 +128,7 @@ mount $EFI /mnt/boot
 # Mount home part
 print "Mount EFI part"
 mkdir /mnt/home
-mount -t zfs ${SYS_ROOT}/${SYSTEM_NAME}/home /mnt/home
+mount -t zfs zroot/data/home /mnt/home
 
 # Copy ZFS cache
 print "Generate and copy zfs cache"
