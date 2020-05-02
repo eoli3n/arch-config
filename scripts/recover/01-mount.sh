eac93b94eb21f185d4b61e566af45b5c23851a7f
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-05-03 01:44:35 +0200

    updated mount script

--- scripts/recover/01-mount.sh
+++ scripts/recover/01-mount.sh
@@ -12,17 +12,14 @@ zpool import -d /dev/disk/by-id -R /mnt -l zroot
 
 # Enable SWAP
 print "Enable SWAP"
-mkswap -f /dev/zvol/zroot/swap
 swapon /dev/zvol/zroot/swap
 
 # Mount EFI part
 print "Mount EFI part"
-mkdir /mnt/boot
 mount $EFI /mnt/boot
 
 # Mount home part
 print "Mount EFI part"
-mkdir /mnt/home
 mount -t zfs zroot/data/home /mnt/home
 
 # Finish
