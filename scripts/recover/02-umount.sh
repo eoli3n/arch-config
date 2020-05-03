07c39e543fef40a3dfca73336ef18347fb95df74
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-05-03 18:14:23 +0200

    udpated umount script

--- scripts/recover/02-umount.sh
+++ scripts/recover/02-umount.sh
@@ -4,17 +4,8 @@ print () {
     echo -e "\n\033[1m> $1\033[0m\n"
 }
 
-print "Disable SWAP"
-swapoff /dev/zvol/zroot/swap
-
 print "Umount /boot"
 umount /mnt/boot
-
-print "Umount slash dataset"
-zfs zroot/ROOT/default
-
-print "Umount zfs"
-zfs umount -a
 
 print "Export zpool"
 zpool export zroot
