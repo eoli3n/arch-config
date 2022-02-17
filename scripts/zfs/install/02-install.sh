b6eccc32dbee4ea65c37ce823da0dc65e26ff362
eoli3n <jonathan.kirszling@runbox.com>
2022-02-17 14:21:44 +0100

    switch lines to install archlinux-keyring with a keyring initialised

--- scripts/zfs/install/02-install.sh
+++ scripts/zfs/install/02-install.sh
@@ -85,8 +85,8 @@ arch-chroot /mnt /bin/bash -xe <<EOF
 
   # ZFS deps
   rm -Rf /etc/pacman.d/gnupg
-  pacman -S archlinux-keyring --noconfirm
   pacman-key --init
+  pacman -S archlinux-keyring --noconfirm
   pacman-key --populate archlinux
   pacman-key --recv-keys F75D9D76 --keyserver keyserver.ubuntu.com
   pacman-key --lsign-key F75D9D76
