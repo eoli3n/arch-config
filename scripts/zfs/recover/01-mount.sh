9fd25977228254c8a1387c8233b0e220671cc910
eoli3n <jonathan.kirszling@runbox.com>
2022-02-19 16:52:37 +0100

    updated mount script

--- scripts/zfs/recover/01-mount.sh
+++ scripts/zfs/recover/01-mount.sh
@@ -6,23 +6,41 @@ print () {
     echo -e "\n\033[1m> $1\033[0m\n"
 }
 
+# Set DISK
+select ENTRY in $(ls /dev/disk/by-id/);
+do
+    DISK="/dev/disk/by-id/$ENTRY"
+    echo "Mounting $ENTRY."
+    break
+done
+
 print "Load ZFS module"
 modprobe zfs
 
-print "Import zpool"
+print "Reimport zpool"
+if zpool status zroot &>/dev/null
+then
+  zpool export zroot
+fi
 zpool import -d /dev/disk/by-id -R /mnt zroot -N -f
 
 print "Load ZFS keys"
 zfs load-key -L prompt zroot
 
-print "Mount slash dataset"
-zfs mount zroot/ROOT/archlinux
+print "Mount ROOT dataset"
+select ENTRY in $(zfs list | awk '/ROOT\// {print $1}')
+do
+    echo "Mount $ENTRY as slash dataset."
+    zfs mount "$ENTRY"
+    break
+done
 
 print "Mount other datasets"
 zfs mount -a
 
 print "Mount EFI part"
-mount /dev/sda1 /mnt/efi
+EFI="$DISK-part1"
+mount "$EFI" /mnt/efi
 
 # Finish
 echo -e "\e[32mAll OK"
