632737df1fd68608fa4dd4f36b8337f5a23b171c
stev leibelt <artodeto@bazzline.net>
2022-08-19 14:47:45 +0200

    added usage of configuration file to ease up the case that you need to run it multiple times. Prefixed all steps outputs with >>:: <<

--- scripts/zfs/install/02-install.sh
+++ scripts/zfs/install/02-install.sh
@@ -24,11 +24,22 @@ print () {
     fi
 }
 
+# Initialize current_step
+install_conf="install.conf"
+
+if [[ -f $install_conf ]];
+then
+  echo ":: Sourcing >>$install_conf<< to use existing configuration."
+  . $install_conf
+else
+  touch $install_conf
+fi
+
 # Root dataset
 root_dataset=$(cat /tmp/root_dataset)
 
 # Sort mirrors
-print "Sort mirrors"
+print ":: Sort mirrors"
 systemctl start reflector
 
 # Install
@@ -39,19 +50,22 @@ else
   microcode_package='intel-ucode'
 fi
 
-echo "Which kernel?"
-ask "0) linux-lts   1) linux?"
-
-if [[ ${REPLY} -eq 1 ]];
+if [[ -z ${kernel+x} ]];
 then
-  is_lts_kernel=1
-  kernel="linux"
-else
-  is_lts_kernel=0
-  kernel="linux-lts"
+  echo ":: Which kernel?"
+  ask "0) linux-lts   1) linux?"
+
+  if [[ ${REPLY} -eq 1 ]];
+  then
+    kernel="linux"
+  else
+    kernel="linux-lts"
+  fi
+
+  echo "kernel=\"${kernel}\"" >> $install_conf
 fi
 
-print "Install Arch Linux"
+print ":: Install Arch Linux"
 pacstrap /mnt         \
   base                \
   base-devel          \
@@ -64,35 +78,89 @@ pacstrap /mnt         \
   git                 \
   ansible
 
+if [[ -z ${zpoolname+x} ]];
+then
+  ask "Please input zpool name. Default is >>zroot<<."
+  zpoolname="${REPLY:-zroot}"
+fi
+
 # Generate fstab excluding ZFS entries
-print "Generate fstab excluding ZFS entries"
-genfstab -U /mnt | grep -v "zroot" | tr -s '\n' | sed 's/\/mnt//'  > /mnt/etc/fstab
+print ":: Generate fstab excluding ZFS entries"
+genfstab -U /mnt | grep -v "$zpoolname" | tr -s '\n' | sed 's/\/mnt//'  > /mnt/etc/fstab
  
 # Set hostname
-read -r -p 'Please enter hostname : ' hostname
-echo "$hostname" > /mnt/etc/hostname
+if [[ -z ${hostname+x} ]];
+then
+  read -r -p 'Please enter hostname : ' hostname
+  echo "$hostname" > /mnt/etc/hostname
+
+  echo "hostname=\"${hostname}\"" >> $install_conf
+fi
 
 # Configure /etc/hosts
-print "Configure hosts file"
-cat > /mnt/etc/hosts <<EOF
-#<ip-address>	<hostname.domain.org>	<hostname>
-127.0.0.1	    localhost   	        $hostname
-::1   		    localhost              	$hostname
+print ":: Configure hosts file"
+  cat > /mnt/etc/hosts <<EOF
+  #<ip-address>	<hostname.domain.org>	<hostname>
+  127.0.0.1	    localhost   	        $hostname
+  ::1   		    localhost             $hostname
 EOF
 
 # Prepare locales and keymap
-ask "Which keymap to use ? (fr, de ...)"
-keymap="$REPLY"
-print "Prepare locales and keymap"
+if [[ -z ${keymap+x} ]];
+then
+  print ":: Prepare locales and keymap"
+  echo "Which keymap do you want to use?"
+  ask "0) fr   1) de-latin1   2) input your own"
+
+  case ${REPLY} in
+    0)
+      keymap="fr"
+      ;;
+    1)
+      keymap="de-latin1"
+      ;;
+    2)
+      ask "Please insert your keymap"
+      keymap="${REPLY}"
+      ;;
+    *)
+      keymap="fr"
+      ;;
+  esac
+  echo "keymap=\"${keymap}\"" >> $install_conf
+fi
+
 echo "KEYMAP=$keymap" > /mnt/etc/vconsole.conf
 
-ask "Which locales to use ? (fr_FR, de_DE ...)"
-locale="$REPLY"
-sed -i 's/#\('"$locale"'.UTF-8\)/\1/' /mnt/etc/locale.gen
-echo 'LANG="'"$locale"'.UTF-8"' > /mnt/etc/locale.conf
+if [[ -z ${locale+x} ]];
+then
+  echo "Which locales to use?"
+  ask "0) fr_FR   1) de_DE   2) input your own"
+
+  case ${REPLY} in
+    0)
+      locale="fr_FR"
+      ;;
+    1)
+      locale="de_DE"
+      ;;
+    2)
+      ask "Please insert your keymap"
+      locale="${REPLY}"
+      ;;
+    *)
+      locale="fr_FR"
+      ;;
+  esac
+
+  sed -i 's/#\('"$locale"'.UTF-8\)/\1/' /mnt/etc/locale.gen
+  echo 'LANG="'"$locale"'.UTF-8"' > /mnt/etc/locale.conf
+
+  echo "locale=\"${locale}\"" >> $install_conf
+fi
 
 # Prepare initramfs
-print "Prepare initramfs"
+print ":: Prepare initramfs"
 if lspci | grep ' VGA ' | grep -q -i intel
 then
   modules="i915 intel_agp"
@@ -102,12 +170,12 @@ fi
 cat > /mnt/etc/mkinitcpio.conf <<"EOF"
 MODULES=($modules)
 BINARIES=()
-FILES=(/etc/zfs/zroot.key)
+FILES=(/etc/zfs/$zpoolname.key)
 HOOKS=(base udev autodetect modconf block keyboard keymap zfs filesystems)
 COMPRESSION="zstd"
 EOF
 
-if [[ $is_lts_kernel -eq 0 ]];
+if [[ $kernel = "linux-lts" ]];
 then
   cat > /mnt/etc/mkinitcpio.d/linux-lts.preset <<"EOF"
 ALL_config="/etc/mkinitcpio.conf"
@@ -124,22 +192,59 @@ default_image="/boot/initramfs-linux.img"
 EOF
 fi
 
-print "Copy ZFS files"
+print ":: Copy ZFS files"
 cp /etc/hostid /mnt/etc/hostid
 cp /etc/zfs/zpool.cache /mnt/etc/zfs/zpool.cache
-cp /etc/zfs/zroot.key /mnt/etc/zfs
+cp /etc/zfs/$zpoolname.key /mnt/etc/zfs
 
 ### Configure username
-print 'Set your username'
-read -r -p "Username: " user
+if [[ -z ${user+x} ]];
+then
+  ask "Please input your username"
+  user="${REPLY}"
+
+  echo "user=\"${user}\"" >> $install_conf
+fi
+
+if [[ -d /mnt/home/$user ]];
+then
+  ask "User exists, delete it? (y|N)"
+
+  if [[ ${REPLY} =~ ^[Yy]$ ]];
+  then
+    arch-chroot /mnt /bin/bash -xe "userdel $user"
+  fi
+fi
+
+if [[ -z ${timezone+x} ]];
+then
+  echo "What is your timezone?"
+  ask "0) Europe/Paris   1) Europe/Berlin   2) input your own"
+
+  case ${REPLY} in
+    0)
+      timezone="Europe/Paris"
+      ;;
+    1)
+      timezone="Europe/Berlin"
+      ;;
+    2)
+      ask "Please insert your keymap"
+      timezone="${REPLY}"
+      ;;
+    *)
+      timezone="Europe/Paris"
+      ;;
+  esac
+
+  echo "timezone=\"${timezone}\"" >> $install_conf
+fi
+
+print ":: Configure timezone"
+timedatectl set-ntp true
 
 # Chroot and configure
-print "Chroot and configure system"
-
-# Set date
-ask "What is your timezone? Europe/Paris, Europe/Berlin ..."
-timezone="$REPLY"
-timedatectl set-ntp true
+print ":: Chroot and configure system"
 
 arch-chroot /mnt /bin/bash -xe <<EOF
 
@@ -154,7 +259,12 @@ arch-chroot /mnt /bin/bash -xe <<EOF
   pacman-key --lsign-key F75D9D76
   pacman -S archlinux-keyring --noconfirm
   # https://wiki.archlinux.org/title/Unofficial_user_repositories#archzfs
-  cat >> /etc/pacman.conf <<"EOSF"
+
+  #### Check if an archzfs entries exists alreads in /etc/pacman.conf
+  # We only want to add it once
+  if ! grep -q 'archzfs' < /etc/pacman.conf;
+  then
+    cat >> /etc/pacman.conf <<"EOSF"
 [archzfs]
 # Origin Server - France
 Server = http://archzfs.com/archzfs/x86_64
@@ -167,6 +277,7 @@ Server = https://mirror.in.themindsmaze.com/archzfs/archzfs/x86_64
 # Mirror - US
 Server = https://zxcvfdsa.com/archzfs/archzfs/x86_64
 EOSF
+  fi
   pacman -Syu --noconfirm zfs-dkms zfs-utils
 
   # Set date
@@ -180,7 +291,7 @@ EOSF
   source /etc/locale.conf
   
   # Set keyboard layout
-  echo "KEYMAP=de-latin1" > /etc/vconsole.con
+  echo "KEYMAP=$keymap" > /etc/vconsole.conf
 
   # Generate Initramfs
   mkinitcpio -P
@@ -199,15 +310,15 @@ EOSF
 EOF
 
 # Set root passwd
-print "Set root password"
+print ":: Set root password"
 arch-chroot /mnt /bin/passwd
 
 # Set user passwd
-print "Set user password"
+print ":: Set user password"
 arch-chroot /mnt /bin/passwd "$user"
 
 # Configure sudo
-print "Configure sudo"
+print ":: Configure sudo"
 cat > /mnt/etc/sudoers <<EOF
 root ALL=(ALL) ALL
 $user ALL=(ALL) ALL
@@ -215,20 +326,26 @@ Defaults rootpw
 EOF
 
 # Configure network
-ask "Configure networking ?"
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ -z ${configure_network+x} ]];
 then
+  ask "Configure networking? (y|N)"
+  if [[ $REPLY =~ ^[Yy]$ ]];
+  then
+    echo "Which network-provider?"
+    ask "0) iwd + wpa_supplicant   1) networkmanager"
 
-echo "Which network-provider?"
-ask "0) iwd + wpa_supplicant   1) networkmanager"
-if [[ ${REPLY} -eq 1 ]];
+    echo "configure_network=\"${REPLY}\"" >> $install_conf
+  fi
+fi
+
+if [[ $configure_network -eq 1]];
 then
   pacstrap /mnt         \
     networkmanager
 
   systemctl enable NetworkManager.service --root=/mnt
-
-else
+elif [[ $configure_network -eq 0 ]];
+then
   pacstrap /mnt         \
     iwd                 \
     wpa_supplicant
@@ -267,12 +384,22 @@ UseDefaultInterface=true
 EnableNetworkConfiguration=true
 EOF
     systemctl enable iwd --root=/mnt
-  fi
+else
+  echo ":: No network configured!"
+  echo "   You have to do it manually or you wont have any network that easily on your new installed arch linux."
 fi
 
 # Configure DNS
-ask "Configure DNS ?"
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ -z ${configure_dns+x} ]];
+then
+  ask "Configure DNS? (y|N)"
+
+  if [[ $REPLY =~ ^[Yy]$ ]]
+    echo "configure_dns=1" >> $install_conf
+  fi
+fi
+
+if [[ $configure_dns -eq 1 ]];
 then
   rm /mnt/etc/resolv.conf
   ln -s /run/systemd/resolve/resolv.conf /mnt/etc/resolv.conf
@@ -281,24 +408,24 @@ then
 fi
 
 # Activate zfs
-print "Configure ZFS"
+print ":: Configure ZFS"
 systemctl enable zfs-import-cache --root=/mnt
 systemctl enable zfs-mount --root=/mnt
 systemctl enable zfs-import.target --root=/mnt
 systemctl enable zfs.target --root=/mnt
 
 # Configure zfs-mount-generator
-print "Configure zfs-mount-generator"
+print ":: Configure zfs-mount-generator"
 mkdir -p /mnt/etc/zfs/zfs-list.cache
-touch /mnt/etc/zfs/zfs-list.cache/zroot
-zfs list -H -o name,mountpoint,canmount,atime,relatime,devices,exec,readonly,setuid,nbmand | sed 's/\/mnt//' > /mnt/etc/zfs/zfs-list.cache/zroot
+touch /mnt/etc/zfs/zfs-list.cache/$zpoolname
+zfs list -H -o name,mountpoint,canmount,atime,relatime,devices,exec,readonly,setuid,nbmand | sed 's/\/mnt//' > /mnt/etc/zfs/zfs-list.cache/$zpoolname
 systemctl enable zfs-zed.service --root=/mnt
 
 # Configure zfsbootmenu
 mkdir -p /mnt/efi/EFI/ZBM
 
 # Generate zfsbootmenu efi
-print 'Configure zfsbootmenu'
+print ":: Configure zfsbootmenu"
 # https://github.com/zbm-dev/zfsbootmenu/blob/master/etc/zfsbootmenu/mkinitcpio.conf
 
 cat > /mnt/etc/zfsbootmenu/mkinitcpio.conf <<"EOF"
@@ -327,14 +454,14 @@ Kernel:
 EOF
 
 # Set cmdline
-zfs set org.zfsbootmenu:commandline="rw quiet nowatchdog rd.vconsole.keymap=fr" zroot/ROOT/"$root_dataset"
+zfs set org.zfsbootmenu:commandline="rw quiet nowatchdog rd.vconsole.keymap=fr" $zpoolname/ROOT/"$root_dataset"
 
 # Generate ZBM
-print 'Generate zbm'
+print ":: Generate zbm"
 arch-chroot /mnt /bin/bash -xe <<"EOF"
 
   # Export locale
-  export LANG="fr_FR.UTF-8"
+  export LANG="$locale"
 
   # Generate zfsbootmenu
   generate-zbm
@@ -345,7 +472,7 @@ if [[ -f /tmp/disk ]]
 then
   DISK=$(cat /tmp/disk)
 else
-  print 'Select the disk you installed on:'
+  print ":: Select the disk you installed on:"
   select ENTRY in $(ls /dev/disk/by-id/);
   do
       DISK="/dev/disk/by-id/$ENTRY"
@@ -355,7 +482,7 @@ else
 fi
 
 # Create UEFI entries
-print 'Create efi boot entries'
+print ":: Create efi boot entries"
 if ! efibootmgr | grep ZFSBootMenu
 then
     efibootmgr --disk "$DISK" \
@@ -371,17 +498,17 @@ then
       --loader "\EFI\ZBM\vmlinuz.efi" \
       --verbose
 else
-    print 'Boot entries already created'
+    print ":: Boot entries already created"
 fi
 
 # Umount all parts
-print "Umount all parts"
+print ":: Umount all parts"
 umount /mnt/efi
 zfs umount -a
 
 # Export zpool
-print "Export zpool"
-zpool export zroot
+print ":: Export zpool"
+zpool export $zpoolname
 
 # Finish
 echo -e "\e[32mAll OK"
